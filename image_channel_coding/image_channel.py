1e9f53b91c9741343ce19702022953dc760f2477
coadylewis <coadylewis@gmail.com>
2022-08-21 23:50:42 -0500

    started hamming code prelim

--- image_channel_coding/image_channel.py
+++ image_channel_coding/image_channel.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from numpy import zeros, log2, array
+from numpy import zeros, log2, array, concatenate, split
 from math import floor
 from random import random
 from time import time
@@ -57,6 +57,44 @@ def repetition_decoder(x, n):
 	return output
 
 
+def is_power_2(x):
+	check = round(log2(x),5)
+	return check.is_integer()
+
+
+def list_2d_to_bin_list(data): # convert 2d array of ints to a single binary list
+	output=int_to_bin_8bit(data[0][0][0])
+	#print(output)
+	for i in range(len(data)): #row
+		for j in range(len(data[i])): #column
+			for k in range(len(data[i][j])):
+				if(i==0 and j==0 and k==0):
+					continue
+				#print(int_to_bin_8bit(data[i][j][k]))
+				output=concatenate((output,int_to_bin_8bit(data[i][j][k])))
+	print('successful')
+	return output
+
+
+def list_to_bin_2d_list(x,num_bits,width,height): # inverse of above function
+	data_1d = split(x,len(x)//num_bits)
+	data = []
+	for i in range(height):
+		data.append([])
+		for j in range(width):
+			data[i].append([])#i*j+j)
+			for k in range(3):
+				data[i][j].append(bin_8bit_to_int(data_1d[i*width*3+j*3+k]))
+	return data
+
+
+def hamming_encoder(x, num_blocks): 
+	k=len(x) # num information bits
+	# soln. here
+
+
+
+
 
 # load the image
 
@@ -66,24 +104,49 @@ image = Image.open(input_image_path)
 
 data = array(image)
 
+bits = 8
 
-# # modify image with basic channel
+
+# # test transform to 1d list and back
+# w=10
+# h=10
+# data_subset=data[:w][:h]
+# t1=time()
+# data_subset_transformed = list_to_bin_2d_list(list_2d_to_bin_list(data_subset),bits,w,h)
+# print(time()-t1)
+# # for i in range(len(data_subset)):
+# # 	for j in range(len(data_subset[i])):
+# # 		for k in range(len(data_subset[i][j])):
+# # 			print(data_subset[i][j][k]-data_subset_transformed[i][j][k])
+# # 			data_subset[i][j][k]-=data_subset_transformed[i][j][k]
+# print(data_subset)
+# print(array(data_subset_transformed))
+# #Image.fromarray(data).save("basic.jpg")
+
+
+
+
+
+# # # modify image with basic channel
+# # t1=time()
+# # for i in range(len(data)):
+# # 	for j in range(len(data[i])):
+# # 		for k in range(len(data[i][j])):
+# # 			data[i][j][k] = bin_8bit_to_int(received(int_to_bin_8bit(data[i][j][k])))
+# # print(time()-t1)
+# # Image.fromarray(data).save("basic.jpg")
+
+
+
+
+
+# # modify image with repetition encoded channel
+# rep = 7
 # t1=time()
 # for i in range(len(data)):
 # 	for j in range(len(data[i])):
 # 		for k in range(len(data[i][j])):
-# 			data[i][j][k] = bin_8bit_to_int(received(int_to_bin_8bit(data[i][j][k])))
+# 			data[i][j][k] = bin_8bit_to_int(repetition_decoder(received(repetition_encoder(int_to_bin_8bit(data[i][j][k]), rep)), rep))
 # print(time()-t1)
-# Image.fromarray(data).save("basic.jpg")
-
-
-# modify image with repetition encoded channel
-rep = 7
-t1=time()
-for i in range(len(data)):
-	for j in range(len(data[i])):
-		for k in range(len(data[i][j])):
-			data[i][j][k] = bin_8bit_to_int(repetition_decoder(received(repetition_encoder(int_to_bin_8bit(data[i][j][k]), rep)), rep))
-print(time()-t1)
-Image.fromarray(data).save("repetition_encoding.jpg")
+# Image.fromarray(data).save("repetition_encoding.jpg")
 
